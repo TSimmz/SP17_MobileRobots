e9f61503ed40bacc67f7a71be831d0b5ab97641d
TSimmz <tyler.simoni.8@gmail.com>
2017-07-21 13:10:06 -0400

    Lab 4

---
+++ Lab 4/Task02_Maze/Maze.h
@@ -0,0 +1,38 @@
+#ifndef __MAZE
+#define __MAZE
+
+#include "Position.h"
+
+const Position NULLPOS(-1,-1);
+
+using namespace std;
+
+typedef enum{OPEN, WALL, VISITED, RED, BLUE} state;
+
+state **getMazeArray(int size);
+
+class Maze
+{
+  public:
+   static const int MAZESIZE = 9; // default maze size
+ 
+   Maze();
+
+   void init_Maze1(); 
+   void init_Maze2(); 
+   void init_Maze3(); 
+  
+   state getState(const Position &P) const;
+   void setState(const Position &P, state s);
+
+   //void print_dfsExitPath();
+   
+   int size;
+   Position start;
+   Position exitpos;
+   state **M;         // two-dimensional array of state values
+};
+
+//void printBottomToTop(stack<Position> S);
+
+#endif
---
+++ Lab 4/Task02_Maze/Position.cpp
@@ -0,0 +1,67 @@
+#include "Position.h"
+
+using namespace std;
+
+direction next_dirR(direction d)
+{
+   switch(d)
+   {
+      case SOUTH: return WEST;
+      case WEST: return NORTH;
+      case NORTH: return EAST;
+      case EAST: return SOUTH;
+   }
+}
+
+direction next_dirL(direction d)
+{
+   switch(d)
+   {
+      case SOUTH: return EAST;
+      case WEST: return SOUTH;
+      case NORTH: return WEST;
+      case EAST: return NORTH;
+   }
+}
+
+direction next_direction(direction d)
+{
+  switch(d)
+  {
+    case EAST: return SOUTH;
+    case SOUTH: return WEST;
+    case WEST: return NORTH;
+    case NORTH: return NONE;
+    
+  }
+}
+
+direction opposite_dir(direction d)
+{
+  switch(d)
+  {
+    case EAST: return WEST;
+    case SOUTH: return NORTH;
+    case WEST: return EAST;
+    case NORTH: return SOUTH;
+  }
+}
+
+Position Position::Neighbor(direction d, int i) const
+{
+   switch(d) {
+      case SOUTH:
+	      return Position(col,row+i);
+      case WEST:
+	      return Position(col-i,row);
+      case NORTH:
+	      return Position(col,row-i);
+      case EAST:
+	      return Position(col+i,row);
+   }
+}
+
+bool Position::operator==(const Position &other) const
+{
+   return row == other.row && col == other.col;
+}
